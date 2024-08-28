acfd8d6740c2e3ade039ae442b8e45db2095aadf
Mihai Spinei <mspinei@betterbe.com>
2024-08-28 10:01:19 +0200

    local

--- init.lua
+++ init.lua
@@ -32,6 +32,8 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.cmd 'set rtp+=/usr/local/opt/fzf'
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
@@ -58,10 +60,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- is not what someone will guess without a bit more experience.
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -522,33 +524,42 @@ require('lazy').setup({
           goto_node = '<cr>',
           show_help = '?',
         },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = 'tis',
+            node_incremental = 'tii',
+            scope_incremental = 'ti}',
+            node_decremental = 'tid',
+          },
+        },
       },
+      config = function(_, opts)
+        local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
+        require('nvim-treesitter.install').prefer_git = true
+        ---@diagnostic disable-next-line: missing-fields
+        require('nvim-treesitter.configs').setup(opts)
+
+        -- There are additional nvim-treesitter modules that you can use to interact
+        -- with nvim-treesitter. You should go explore a few and see what interests you:
+        --
+        --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+        --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+        --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      end,
     },
-    config = function(_, opts)
-      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
 
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+    require 'kickstart.plugins.debug',
+    require 'kickstart.plugins.indent_line',
+    require 'kickstart.plugins.lint',
+    require 'kickstart.plugins.autopairs',
+    require 'kickstart.plugins.neo-tree',
+    require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    end,
+    -- Custom plugins go here
+    { import = 'custom.plugins' },
   },
-
-  require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- Custom plugins go here
-  { import = 'custom.plugins' },
 }, {
 
   ui = {
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -104,6 +104,18 @@ return {
       vim.cmd 'colorscheme kanagawa-dragon'
     end,
   },
+  {
+    'mfussenegger/nvim-jdtls',
+    config = function()
+      local config = {
+        cmd = { 'jdtls' },
+        root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+      }
+      vim.keymap.set('n', '<leader>jls', function()
+        require('jdtls').start_or_attach(config)
+      end, { desc = 'Start Java Language Server' })
+    end,
+  },
 
   {
     'akinsho/toggleterm.nvim',
@@ -111,6 +123,30 @@ return {
       require('toggleterm').setup {
         open_mapping = [[<c-\>]],
       }
+    end,
+  },
+  {
+    'leath-dub/snipe.nvim',
+    keys = { {
+      '<leader>b',
+      function()
+        require('snipe').open_buffer_menu()
+      end,
+    } },
+    opts = {},
+  },
+  {
+    'vimlab/split-term.vim',
+    config = function()
+      vim.keymap.set('n', '<leader>jt', ':vsplit|terminal ~/jtest r %<CR>', { desc = 'Run current class as junit test' })
+      vim.keymap.set('n', '<leader>jct', ':vsplit|terminal ~/jtest c %<CR>', { desc = 'Run current class as junit test' })
+    end,
+  },
+
+  {
+    'xiyaowong/transparent.nvim',
+    config = function()
+      require('transparent').setup {}
     end,
   },
 
@@ -419,6 +455,121 @@ return {
       }
 
       vim.cmd [[nnoremap <leader><tab> :Neotree toggle<cr>]]
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          hijack_netrw_behaviour = 'open_default',
+        },
+      }
+      vim.keymap.set('n', '<leader><tab>', ':Neotree toggle<CR>', { desc = 'Open file explorer' })
+    end,
+  },
+
+  {
+    'rebelot/kanagawa.nvim',
+    config = function()
+      require('kanagawa').setup {
+        dimInactive = true,
+      }
+      -- vim.cmd 'colorscheme kanagawa-wave'
+    end,
+  },
+
+  {
+    'ellisonleao/gruvbox.nvim',
+    config = function()
+      vim.o.background = 'dark'
+      vim.cmd 'colorscheme gruvbox'
+    end,
+  },
+
+  -- lazy.nvim
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
+          -- you can enable a preset for easier configuration
+          presets = {
+            bottom_search = true, -- use a classic bottom cmdline for search
+            command_palette = true, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false, -- add a border to hover docs and signature help
+          },
+        },
+      }
+    end,
+  },
+
+  {
+    'MunifTanjim/nui.nvim',
+  },
+
+  {
+    'chentoast/marks.nvim',
+    config = function()
+      require('marks').setup {
+        -- whether to map keybinds or not. default true
+        default_mappings = true,
+        -- which builtin marks to show. default {}
+        builtin_marks = { '.', '<', '>', '^' },
+        -- whether movements cycle back to the beginning/end of buffer. default true
+        cyclic = true,
+        -- whether the shada file is updated after modifying uppercase marks. default false
+        force_write_shada = false,
+        -- how often (in ms) to redraw signs/recompute mark positions.
+        -- higher values will have better performance but may cause visual lag,
+        -- while lower values may cause performance penalties. default 150.
+        refresh_interval = 250,
+        -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
+        -- marks, and bookmarks.
+        -- can be either a table with all/none of the keys, or a single number, in which case
+        -- the priority applies to all marks.
+        -- default 10.
+        sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+        -- disables mark tracking for specific filetypes. default {}
+        excluded_filetypes = {},
+        -- disables mark tracking for specific buftypes. default {}
+        excluded_buftypes = {},
+        -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+        -- sign/virttext. Bookmarks can be used to group together positions and quickly move
+        -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+        -- default virt_text is "".
+        bookmark_0 = {
+          sign = '⚑',
+          virt_text = 'hello world',
+          -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
+          -- defaults to false.
+          annotate = false,
+        },
+        mappings = {},
+      }
     end,
   },
 }
