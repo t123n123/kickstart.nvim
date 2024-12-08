7c1ceadf135930028c7abe1689c54141b9eacd27
Mihai Spinei <spineim22@gmail.com>
2024-12-08 18:23:27 +0100

    cleaning up setup

--- lua/kickstart/plugins/oil.lua
+++ lua/kickstart/plugins/oil.lua
@@ -1,30 +1,25 @@
-require("oil").setup({
-  -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-  -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
-  default_file_explorer = true,
-  -- Id is automatically added at the beginning, and name at the end
-  -- See :help oil-columns
+require('oil').setup {
+  default_file_explorer = false,
   columns = {
-    "icon",
-    -- "permissions",
-    -- "size",
+    'icon',
+    'size',
     -- "mtime",
   },
   -- Buffer-local options to use for oil buffers
   buf_options = {
     buflisted = false,
-    bufhidden = "hide",
+    bufhidden = 'hide',
   },
   -- Window-local options to use for oil buffers
   win_options = {
     wrap = false,
-    signcolumn = "no",
+    signcolumn = 'no',
     cursorcolumn = false,
-    foldcolumn = "0",
+    foldcolumn = '0',
     spell = false,
     list = false,
     conceallevel = 3,
-    concealcursor = "nvic",
+    concealcursor = 'nvic',
   },
   -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
   delete_to_trash = false,
@@ -46,7 +41,7 @@ require("oil").setup({
   },
   -- Constrain the cursor to the editable parts of the oil buffer
   -- Set to `false` to disable, or "name" to keep it on the file names
-  constrain_cursor = "editable",
+  constrain_cursor = 'editable',
   -- Set to true to watch the filesystem for changes and reload oil
   watch_for_changes = false,
   -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
@@ -56,22 +51,22 @@ require("oil").setup({
   -- Set to `false` to remove a keymap
   -- See :help oil-actions for a list of all available actions
   keymaps = {
-    ["g?"] = "actions.show_help",
-    ["<CR>"] = "actions.select",
-    ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-    ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
-    ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-    ["<C-p>"] = "actions.preview",
-    ["<C-c>"] = "actions.close",
-    ["<C-l>"] = "actions.refresh",
-    ["-"] = "actions.parent",
-    ["_"] = "actions.open_cwd",
-    ["`"] = "actions.cd",
-    ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
-    ["gs"] = "actions.change_sort",
-    ["gx"] = "actions.open_external",
-    ["g."] = "actions.toggle_hidden",
-    ["g\\"] = "actions.toggle_trash",
+    ['g?'] = 'actions.show_help',
+    ['<CR>'] = 'actions.select',
+    ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+    ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+    ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+    ['<C-p>'] = 'actions.preview',
+    ['<C-c>'] = 'actions.close',
+    ['<C-l>'] = 'actions.refresh',
+    ['-'] = 'actions.parent',
+    ['_'] = 'actions.open_cwd',
+    ['`'] = 'actions.cd',
+    ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+    ['gs'] = 'actions.change_sort',
+    ['gx'] = 'actions.open_external',
+    ['g.'] = 'actions.toggle_hidden',
+    ['g\\'] = 'actions.toggle_trash',
   },
   -- Set to false to disable all of the above keymaps
   use_default_keymaps = true,
@@ -80,7 +75,7 @@ require("oil").setup({
     show_hidden = false,
     -- This function defines what is considered a "hidden" file
     is_hidden_file = function(name, bufnr)
-      return vim.startswith(name, ".")
+      return vim.startswith(name, '.')
     end,
     -- This function defines what will never be shown, even when `show_hidden` is set
     is_always_hidden = function(name, bufnr)
@@ -94,8 +89,8 @@ require("oil").setup({
     sort = {
       -- sort order can be "asc" or "desc"
       -- see :help oil-columns to see which columns are sortable
-      { "type", "asc" },
-      { "name", "asc" },
+      { 'type', 'asc' },
+      { 'name', 'asc' },
     },
   },
   -- Extra arguments to pass to SCP when moving/copying files over SSH
@@ -119,12 +114,12 @@ require("oil").setup({
     padding = 2,
     max_width = 0,
     max_height = 0,
-    border = "rounded",
+    border = 'rounded',
     win_options = {
       winblend = 0,
     },
     -- preview_split: Split direction: "auto", "left", "right", "above", "below".
-    preview_split = "auto",
+    preview_split = 'auto',
     -- This is the config that will be passed to nvim_open_win.
     -- Change values here to customize the layout
     override = function(conf)
@@ -149,7 +144,7 @@ require("oil").setup({
     min_height = { 5, 0.1 },
     -- optionally define an integer/float for the exact height of the preview window
     height = nil,
-    border = "rounded",
+    border = 'rounded',
     win_options = {
       winblend = 0,
     },
@@ -164,19 +159,18 @@ require("oil").setup({
     max_height = { 10, 0.9 },
     min_height = { 5, 0.1 },
     height = nil,
-    border = "rounded",
-    minimized_border = "none",
+    border = 'rounded',
+    minimized_border = 'none',
     win_options = {
       winblend = 0,
     },
   },
   -- Configuration for the floating SSH window
   ssh = {
-    border = "rounded",
+    border = 'rounded',
   },
   -- Configuration for the floating keymaps help window
   keymaps_help = {
-    border = "rounded",
+    border = 'rounded',
   },
-})
-
+}
